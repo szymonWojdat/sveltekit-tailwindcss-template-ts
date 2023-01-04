9a88a9b3b38e7dfb6d4d3be591654ee02b941fc6
Szymon Wojdat <szymonwojdat13@gmail.com>
2023-01-04 16:00:44 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let a = 123
+</script>
+
+<h1>Welcome to SvelteKit {a}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  let a = 'test 123'
+</script>
+
+<h1>Test about page {a}</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
