f6694fe5551a198b057fa4943943b76afba50a2b
Szymon Wojdat <szymonwojdat13@gmail.com>
2023-01-09 10:47:10 +0000

    Add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,5 @@
   let a = 123
 </script>
 
-<h1>Welcome to SvelteKit {a}</h1>
+<h1 class="text-5xl text-blue-700">Welcome to SvelteKit {a}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
